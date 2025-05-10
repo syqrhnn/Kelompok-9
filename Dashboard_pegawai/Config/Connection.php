5575a58f1176fdb1d010112d63733517c9f7fe24
rehan syauqi <syqrhnn@gmail.com>
2025-05-11 00:11:38 +0700

    Update Database

--- Dashboard_pegawai/Config/Connection.php
+++ Dashboard_pegawai/Config/Connection.php
@@ -1,14 +1,15 @@
 <?php
 $host = 'localhost';
-$dbname = 'db_rental_mobil';       // Nama database kamu
-$username = 'root';    // Username (XAMPP default)
-$password = '';        // Password (kosong jika default)
+$dbname = 'carrenta_db_rental_mobil';  // Nama database baru
+$username = 'carrenta_db_rental_mobil'; // Username baru
+$password = 'wyYSE9smKvQ8JJFAM5wr';     // Password baru
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
+    echo "Koneksi berhasil.";
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
 }
